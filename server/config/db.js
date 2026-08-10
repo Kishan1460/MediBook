98e66aeb11ccf8dd7a634a7f4a168cf9a2de5e06
“kishan1460” <kishanbisht191@gmail.comgit config --global user.email kishanbisht191@gmail.com>
2026-08-10 10:42:40 +0530

    new commit

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/doctor-appointment";
+    const uri = process.env.MONGO_URI;
     await mongoose.connect(uri);
     console.log("MongoDB connected");
   } catch (error) {
